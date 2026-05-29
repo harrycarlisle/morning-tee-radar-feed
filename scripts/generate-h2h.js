842418332ccf7b34bf018c29912e06f3369a52e0
harrycarlisle <63021848+harrycarlisle@users.noreply.github.com>
2026-05-29 18:46:49 -0400

    Update generate-h2h.js

--- scripts/generate-h2h.js
+++ scripts/generate-h2h.js
@@ -10,174 +10,154 @@ if (!API_KEY) {
 const TOUR = "pga";
 const CURRENT_YEAR = new Date().getFullYear();
 
+const PLAYERS = {
+  scottie: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
+  rory: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
+  bryson: { name: "DeChambeau, Bryson", displayName: "Bryson DeChambeau", slug: "bryson-dechambeau", dgId: 19841, country: "USA" },
+  brooks: { name: "Koepka, Brooks", displayName: "Brooks Koepka", slug: "brooks-koepka", dgId: 16243, country: "USA" },
+  rahm: { name: "Rahm, Jon", displayName: "Jon Rahm", slug: "jon-rahm", dgId: 19195, country: "ESP" },
+  xander: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
+  collin: { name: "Morikawa, Collin", displayName: "Collin Morikawa", slug: "collin-morikawa", dgId: 22085, country: "USA" },
+  ludvig: { name: "Aberg, Ludvig", displayName: "Ludvig Åberg", slug: "ludvig-aberg", dgId: 23950, country: "SWE" },
+  viktor: { name: "Hovland, Viktor", displayName: "Viktor Hovland", slug: "viktor-hovland", dgId: 18841, country: "NOR" },
+  tommy: { name: "Fleetwood, Tommy", displayName: "Tommy Fleetwood", slug: "tommy-fleetwood", dgId: 12294, country: "ENG" },
+  phil: { name: "Mickelson, Phil", displayName: "Phil Mickelson", slug: "phil-mickelson", dgId: 1547, country: "USA" },
+  tiger: { name: "Woods, Tiger", displayName: "Tiger Woods", slug: "tiger-woods", dgId: 5321, country: "USA" },
+
+  jt: { name: "Thomas, Justin", displayName: "Justin Thomas", slug: "justin-thomas", dgId: 14139, country: "USA" },
+  spieth: { name: "Spieth, Jordan", displayName: "Jordan Spieth", slug: "jordan-spieth", dgId: 14636, country: "USA" },
+  cantlay: { name: "Cantlay, Patrick", displayName: "Patrick Cantlay", slug: "patrick-cantlay", dgId: 15466, country: "USA" },
+  hideki: { name: "Matsuyama, Hideki", displayName: "Hideki Matsuyama", slug: "hideki-matsuyama", dgId: 13562, country: "JPN" },
+  shane: { name: "Lowry, Shane", displayName: "Shane Lowry", slug: "shane-lowry", dgId: 13900, country: "IRL" },
+  camSmith: { name: "Smith, Cameron", displayName: "Cameron Smith", slug: "cameron-smith", dgId: 15856, country: "AUS" },
+  dj: { name: "Johnson, Dustin", displayName: "Dustin Johnson", slug: "dustin-johnson", dgId: 12422, country: "USA" },
+  reed: { name: "Reed, Patrick", displayName: "Patrick Reed", slug: "patrick-reed", dgId: 14838, country: "USA" },
+  sam: { name: "Burns, Sam", displayName: "Sam Burns", slug: "sam-burns", dgId: 19483, country: "USA" },
+  max: { name: "Homa, Max", displayName: "Max Homa", slug: "max-homa", dgId: 17538, country: "USA" },
+  wyndham: { name: "Clark, Wyndham", displayName: "Wyndham Clark", slug: "wyndham-clark", dgId: 23604, country: "USA" },
+  tony: { name: "Finau, Tony", displayName: "Tony Finau", slug: "tony-finau", dgId: 11676, country: "USA" },
+  sahith: { name: "Theegala, Sahith", displayName: "Sahith Theegala", slug: "sahith-theegala", dgId: 23014, country: "USA" },
+  corey: { name: "Conners, Corey", displayName: "Corey Conners", slug: "corey-conners", dgId: 17576, country: "CAN" },
+  fitz: { name: "Fitzpatrick, Matt", displayName: "Matt Fitzpatrick", slug: "matt-fitzpatrick", dgId: 17646, country: "ENG" }
+};
+
+function p(key) {
+  return PLAYERS[key];
+}
+
+function m(playerA, playerB, startYear = 2020, dataNote = null) {
+  return {
+    playerA: p(playerA),
+    playerB: p(playerB),
+    startYear,
+    ...(dataNote ? { dataNote } : {})
+  };
+}
+
 const MATCHUPS = [
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "DeChambeau, Bryson", displayName: "Bryson DeChambeau", slug: "bryson-dechambeau", dgId: 19841, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "Rahm, Jon", displayName: "Jon Rahm", slug: "jon-rahm", dgId: 19195, country: "ESP" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "Morikawa, Collin", displayName: "Collin Morikawa", slug: "collin-morikawa", dgId: 22085, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "Hovland, Viktor", displayName: "Viktor Hovland", slug: "viktor-hovland", dgId: 18841, country: "NOR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    playerB: { name: "Fleetwood, Tommy", displayName: "Tommy Fleetwood", slug: "tommy-fleetwood", dgId: 12294, country: "ENG" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Aberg, Ludvig", displayName: "Ludvig Åberg", slug: "ludvig-aberg", dgId: 23950, country: "SWE" },
-    playerB: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    startYear: 2023
-  },
-  {
-    playerA: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    playerB: { name: "Rahm, Jon", displayName: "Jon Rahm", slug: "jon-rahm", dgId: 19195, country: "ESP" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    playerB: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    playerB: { name: "Morikawa, Collin", displayName: "Collin Morikawa", slug: "collin-morikawa", dgId: 22085, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    playerB: { name: "Aberg, Ludvig", displayName: "Ludvig Åberg", slug: "ludvig-aberg", dgId: 23950, country: "SWE" },
-    startYear: 2023
-  },
-  {
-    playerA: { name: "Hovland, Viktor", displayName: "Viktor Hovland", slug: "viktor-hovland", dgId: 18841, country: "NOR" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Fleetwood, Tommy", displayName: "Tommy Fleetwood", slug: "tommy-fleetwood", dgId: 12294, country: "ENG" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "DeChambeau, Bryson", displayName: "Bryson DeChambeau", slug: "bryson-dechambeau", dgId: 19841, country: "USA" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "DeChambeau, Bryson", displayName: "Bryson DeChambeau", slug: "bryson-dechambeau", dgId: 19841, country: "USA" },
-    playerB: { name: "Koepka, Brooks", displayName: "Brooks Koepka", slug: "brooks-koepka", dgId: 16243, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "DeChambeau, Bryson", displayName: "Bryson DeChambeau", slug: "bryson-dechambeau", dgId: 19841, country: "USA" },
-    playerB: { name: "Rahm, Jon", displayName: "Jon Rahm", slug: "jon-rahm", dgId: 19195, country: "ESP" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "DeChambeau, Bryson", displayName: "Bryson DeChambeau", slug: "bryson-dechambeau", dgId: 19841, country: "USA" },
-    playerB: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Koepka, Brooks", displayName: "Brooks Koepka", slug: "brooks-koepka", dgId: 16243, country: "USA" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Koepka, Brooks", displayName: "Brooks Koepka", slug: "brooks-koepka", dgId: 16243, country: "USA" },
-    playerB: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Rahm, Jon", displayName: "Jon Rahm", slug: "jon-rahm", dgId: 19195, country: "ESP" },
-    playerB: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Rahm, Jon", displayName: "Jon Rahm", slug: "jon-rahm", dgId: 19195, country: "ESP" },
-    playerB: { name: "Morikawa, Collin", displayName: "Collin Morikawa", slug: "collin-morikawa", dgId: 22085, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Morikawa, Collin", displayName: "Collin Morikawa", slug: "collin-morikawa", dgId: 22085, country: "USA" },
-    playerB: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Morikawa, Collin", displayName: "Collin Morikawa", slug: "collin-morikawa", dgId: 22085, country: "USA" },
-    playerB: { name: "Hovland, Viktor", displayName: "Viktor Hovland", slug: "viktor-hovland", dgId: 18841, country: "NOR" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Aberg, Ludvig", displayName: "Ludvig Åberg", slug: "ludvig-aberg", dgId: 23950, country: "SWE" },
-    playerB: { name: "Schauffele, Xander", displayName: "Xander Schauffele", slug: "xander-schauffele", dgId: 19895, country: "USA" },
-    startYear: 2023
-  },
-  {
-    playerA: { name: "Conners, Corey", displayName: "Corey Conners", slug: "corey-conners", dgId: 17576, country: "CAN" },
-    playerB: { name: "Fitzpatrick, Matt", displayName: "Matt Fitzpatrick", slug: "matt-fitzpatrick", dgId: 17646, country: "ENG" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Conners, Corey", displayName: "Corey Conners", slug: "corey-conners", dgId: 17576, country: "CAN" },
-    playerB: { name: "Fleetwood, Tommy", displayName: "Tommy Fleetwood", slug: "tommy-fleetwood", dgId: 12294, country: "ENG" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Fitzpatrick, Matt", displayName: "Matt Fitzpatrick", slug: "matt-fitzpatrick", dgId: 17646, country: "ENG" },
-    playerB: { name: "Fleetwood, Tommy", displayName: "Tommy Fleetwood", slug: "tommy-fleetwood", dgId: 12294, country: "ENG" },
-    startYear: 2020
-  },
-  {
-    playerA: { name: "Woods, Tiger", displayName: "Tiger Woods", slug: "tiger-woods", dgId: 5321, country: "USA" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2017,
-    dataNote: "Limited to available DataGolf event data from 2017 onward, so this does not capture Tiger's full prime."
-  },
-  {
-    playerA: { name: "Woods, Tiger", displayName: "Tiger Woods", slug: "tiger-woods", dgId: 5321, country: "USA" },
-    playerB: { name: "Mickelson, Phil", displayName: "Phil Mickelson", slug: "phil-mickelson", dgId: 1547, country: "USA" },
-    startYear: 2017,
-    dataNote: "Limited to available DataGolf event data from 2017 onward, so this does not capture the full Tiger vs Phil era."
-  },
-  {
-    playerA: { name: "Mickelson, Phil", displayName: "Phil Mickelson", slug: "phil-mickelson", dgId: 1547, country: "USA" },
-    playerB: { name: "McIlroy, Rory", displayName: "Rory McIlroy", slug: "rory-mcilroy", dgId: 10091, country: "NIR" },
-    startYear: 2017,
-    dataNote: "Limited to available DataGolf event data from 2017 onward."
-  },
-  {
-    playerA: { name: "Woods, Tiger", displayName: "Tiger Woods", slug: "tiger-woods", dgId: 5321, country: "USA" },
-    playerB: { name: "Scheffler, Scottie", displayName: "Scottie Scheffler", slug: "scottie-scheffler", dgId: 18417, country: "USA" },
-    startYear: 2020,
-    dataNote: "Limited to shared starts during Scottie Scheffler's PGA Tour era."
-  }
+  m("scottie", "rory", 2020),
+  m("scottie", "bryson", 2020),
+  m("scottie", "xander", 2020),
+  m("scottie", "rahm", 2020),
+  m("scottie", "collin", 2020),
+  m("scottie", "viktor", 2020),
+  m("scottie", "tommy", 2020),
+  m("scottie", "brooks", 2020),
+  m("scottie", "jt", 2020),
+  m("scottie", "spieth", 2020),
+  m("scottie", "cantlay", 2020),
+  m("scottie", "hideki", 2020),
+  m("scottie", "sam", 2020),
+  m("scottie", "max", 2020),
+  m("scottie", "wyndham", 2020),
+  m("scottie", "tony", 2020),
+
+  m("rory", "bryson", 2020),
+  m("rory", "rahm", 2020),
+  m("rory", "brooks", 2020),
+  m("rory", "xander", 2020),
+  m("rory", "collin", 2020),
+  m("rory", "ludvig", 2023),
+  m("rory", "viktor", 2020),
+  m("rory", "tommy", 2020),
+  m("rory", "jt", 2020),
+  m("rory", "spieth", 2020),
+  m("rory", "cantlay", 2020),
+  m("rory", "hideki", 2020),
+  m("rory", "shane", 2020),
+  m("rory", "camSmith", 2020),
+  m("rory", "dj", 2020),
+  m("rory", "reed", 2020),
+
+  m("bryson", "brooks", 2020),
+  m("bryson", "rahm", 2020),
+  m("bryson", "xander", 2020),
+  m("bryson", "collin", 2020),
+  m("bryson", "jt", 2020),
+  m("bryson", "spieth", 2020),
+  m("bryson", "camSmith", 2020),
+  m("bryson", "dj", 2020),
+
+  m("rahm", "xander", 2020),
+  m("rahm", "collin", 2020),
+  m("rahm", "brooks", 2020),
+  m("rahm", "viktor", 2020),
+  m("rahm", "jt", 2020),
+  m("rahm", "spieth", 2020),
+
+  m("xander", "collin", 2020),
+  m("xander", "viktor", 2020),
+  m("xander", "ludvig", 2023),
+  m("xander", "jt", 2020),
+  m("xander", "spieth", 2020),
+  m("xander", "cantlay", 2020),
+  m("xander", "hideki", 2020),
+
+  m("collin", "viktor", 2020),
+  m("collin", "ludvig", 2023),
+  m("collin", "jt", 2020),
+  m("collin", "spieth", 2020),
+  m("collin", "cantlay", 2020),
+  m("collin", "hideki", 2020),
+
+  m("jt", "spieth", 2020),
+  m("jt", "hideki", 2020),
+  m("jt", "cantlay", 2020),
+  m("spieth", "hideki", 2020),
+  m("spieth", "cantlay", 2020),
+  m("sam", "sahith", 2020),
+  m("max", "wyndham", 2020),
+  m("corey", "fitz", 2020),
+  m("corey", "tommy", 2020),
+  m("fitz", "tommy", 2020),
+
+  m("tiger", "rory", 2017, "Limited to available DataGolf event data from 2017 onward, so this does not capture Tiger's full prime."),
+  m("tiger", "phil", 2017, "Limited to available DataGolf event data from 2017 onward, so this does not capture the full Tiger vs Phil era."),
+  m("tiger", "scottie", 2020, "Limited to shared starts during Scottie Scheffler's PGA Tour era."),
+  m("tiger", "bryson", 2017, "Limited to available DataGolf event data from 2017 onward."),
+  m("phil", "rory", 2017, "Limited to available DataGolf event data from 2017 onward."),
+  m("phil", "scottie", 2020, "Limited to shared starts during Scottie Scheffler's PGA Tour era."),
+  m("phil", "brooks", 2017, "Limited to available DataGolf event data from 2017 onward."),
+  m("tiger", "rahm", 2017, "Limited to available DataGolf event data from 2017 onward."),
+  m("tiger", "xander", 2017, "Limited to available DataGolf event data from 2017 onward."),
+  m("tiger", "collin", 2020, "Limited to available DataGolf event data from 2020 onward."),
+
+  m("phil", "rahm", 2017, "Limited to available DataGolf event data from 2017 onward."),
+  m("phil", "bryson", 2017, "Limited to available DataGolf event data from 2017 onward."),
+
+  m("ludvig", "rahm", 2023),
+  m("ludvig", "scottie", 2023),
+  m("ludvig", "bryson", 2023),
+  m("ludvig", "brooks", 2023),
+
+  m("brooks", "xander", 2020),
+  m("brooks", "collin", 2020),
+  m("brooks", "viktor", 2020),
+
+  m("rahm", "camSmith", 2020)
 ];
 
-const REQUEST_DELAY_MS = 1700;
+const REQUEST_DELAY_MS = 500;
 const RATE_LIMIT_WAIT_MS = 5 * 60 * 1000 + 15000;
 
 const eventResultsCache = new Map();
